7c5f2fbb6ad248e999d3d8d01dcf4ded1b192ebc
SilpMonk <silpmonk@gmail.com>
2023-05-31 16:45:14 +0530

    react-testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { AppProviders } from "./providers/app-providers";
 import { MuiMode } from "./Components/mui/MuiMode";
+import { CounterTwo } from "./Components/counter-two/CounterTwo";
 
 function App() {
   return (
     <AppProviders>
       <div className="App">
-        <MuiMode />
+        <CounterTwo count={0} />
       </div>
     </AppProviders>
   );
